2471a602cee80d07db8c3b2cf66aad1b34cc9fad
Selim Şahin <selimsahin.sns@gmail.com>
2023-06-24 21:45:16 +0300

    created simple app with a circular on off button and status text

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [isButtonOn, setIsButtonOn] = useState(false);
+
+  const handleButtonPress = () => {
+    setIsButtonOn(!isButtonOn);
+  };
+
+  const buttonBackgroundColor = isButtonOn ? 'red' : 'green';
+  const buttonText = isButtonOn ? 'Off' : 'On';
+  const statusText = isButtonOn ? 'Lights turned on!' : 'Lights turned off!';
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: buttonBackgroundColor }]}
+        onPress={handleButtonPress}
+      >
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </TouchableOpacity>
+      <View style={styles.statusContainer}>
+        <Text style={styles.statusText}>{statusText}</Text>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  statusContainer: {
+    position: 'absolute',
+    bottom: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  statusText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#AAAAAA',
   },
 });
+
+export default App;
